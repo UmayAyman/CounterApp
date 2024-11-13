d9cc74c5662ff15e07920c0bb783fdeeb68b7894
UmayAyman <aimanumme827@gmail.com>
2024-11-13 18:46:46 +0500

    mycounterapp

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Counter from './counter';
+import './style.css';
+
+function App() {
+    return (
+        <div className="counter">
+            <h1 style={{backgroundColor: 'rgb(230, 185, 193)', color: 'black', maxWidth: '100%'}}>
+                My Counter
+            </h1>
+            <Counter />
+        </div>
+    );
+}
+
+export default App;
